c984b2e4f3f1657f058e5559ddecabc6a8b4eb2d
Grant Walker <gwwalker@uci.edu>
2014-01-17 20:48:07 -0800

    Got the key pick up to work with interacting with the door object

---
+++ Code/Assets/Scripts/Our Scripts/DoorScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour {
+
+	bool hasHit;
+
+	// Use this for initialization
+	void Start () {
+		hasHit = false;
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if( col.gameObject.name == "player")
+		{
+			hasHit = true;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D collision)
+	{
+		hasHit = false;
+	}
+
+	public bool CheckCollider()
+	{
+		return hasHit;
+	}
+}
---
+++ Code/Assets/Scripts/Our Scripts/Interaction.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interaction : MonoBehaviour {
+
+	public GameObject level;
+	void OnCollisionStay2D(Collision2D other)
+	{
+		if(Input.GetKey(KeyCode.E) && other.collider.tag == "Player")
+		{
+			renderer.material.color = Color.red;
+		}
+	}
+	/*void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			renderer.material.color = Color.red;
+		}
+	}*/
+	void OnCollisionExit(Collision Other)
+	{
+	}
+
+	void OnTriggerStay2D(Collider2D col ) 
+	{
+		if (Input.GetKey(KeyCode.E) && col.tag == "Player")
+		{
+			level.GetComponent<Level1Uni1>().ChangeKeyValue();
+			Debug.Log ("You picked that shit up!");
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

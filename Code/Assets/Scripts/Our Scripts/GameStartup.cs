3e7e3e39fdc583db03c9fbedae8cba2aa28ab947
Tyler <wakkudesufuu@aol.com>
2014-01-19 14:24:08 -0800

    Added in Menu Scene and Script and All Dat Jazz doe.

---
+++ Code/Assets/Scripts/Our Scripts/GameStartup.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStartup : MonoBehaviour {
+
+	// Use this for initialization
+	//public GUITexture menuScreen;
+	private string [] buttonNames= {"Play","Help","Exit"};
+	private bool[] buttons;
+	int currentSelection = 0;
+	void Awake() {
+		Screen.SetResolution(1024,768,true);
+		//menuScreen = this.gameObject.GetComponent("GUITexture") as GUITexture;
+	}
+
+	void Start () {
+		buttons = new bool[buttonNames.Length];
+		/*int textureWidth = menuScreen.texture.width;
+		int textureHeight = menuScreen.texture.height;
+		int screenWidth = Screen.width;
+		int screenHeight = Screen.height;
+
+		int screenAspectRatio = (screenWidth/screenHeight);
+		int textureAspectRatio = (textureWidth/textureHeight);
+
+		int scaledHeight;
+		int scaledWidth;
+		if (textureAspectRatio <= screenAspectRatio) 
+		{
+			scaledHeight = screenHeight;
+			scaledWidth = (screenHeight * textureAspectRatio);
+		}
+		else 
+		{
+			scaledWidth = screenWidth;
+			scaledHeight = (scaledWidth / textureAspectRatio);
+		}
+		float xPostion = screenWidth / 2 - (scaledWidth/2);
+		menuScreen.pixelInset = new Rect(xPostion, scaledHeight - scaledHeight,scaledWidth,scaledHeight);*/
+	}
+
+	void OnGUI()
+	{
+		for (int i = 0; i < buttonNames.Length; i++) 
+		{
+			GUI.SetNextControlName(buttonNames[i]);
+			//buttons[i] = GUI.Button(new Rect(Screen.width - 100,70 + (20 * i), 80, 20),buttonNames[i]);
+			buttons[i] = GUI.Button(new Rect((Screen.width*5)/8,70 + (60 * i), 350, 50),buttonNames[i]);
+		}
+		if (Input.GetKeyUp(KeyCode.Return)) {
+			buttons[currentSelection] = true;
+		}
+		
+		if (buttons[0]) {
+			Application.LoadLevel(2);
+		}
+		if (buttons[1]) {
+			Debug.Log("Second button pressed");
+		}
+		if (buttons[2]) {
+			Debug.Log("Third button pressed");
+			Application.Quit();
+		}
+		Debug.Log(currentSelection);
+		GUI.FocusControl(buttonNames[currentSelection]);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.S)) {
+			if (currentSelection < buttons.Length-1) {
+				currentSelection++;
+			}
+			else 
+				currentSelection = buttons.Length-1;
+		}
+		if (Input.GetKeyUp(KeyCode.W)) {
+			if (currentSelection > 0) {
+				currentSelection--;
+			}
+			else {
+				currentSelection = 0;
+			}
+		}
+	}
+}

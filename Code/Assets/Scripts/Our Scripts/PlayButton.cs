f924cff840296357f2ac3ccba867f9411d1d02c7
Derrick Chuong <chuongd@uci.edu>
2014-01-18 22:55:07 -0800

    Recommitting Platform move

---
+++ Code/Assets/Scripts/Our Scripts/PlayButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButton: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter() {
+		renderer.material.color = Color.red;
+	}
+
+	void OnMouseExit() {
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseDown() {
+		//load application
+		Debug.Log ("button is press");
+	}
+}
